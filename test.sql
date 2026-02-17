91dd45a5e200b09df414ba25eacb8f9b7a2695b5
mxgrtdata <max08.guerout@gmail.com>
2026-02-16 23:26:38 -0500

    Delete test.sql

--- test.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE TABLE `data-technical-cases.mguerout.salesforce_account_clean_ex1` AS
-SELECT *
-FROM `data-technical-cases.da_case.salesforce_Account`
-WHERE Paid_Solutions__c IS NOT NULL;
