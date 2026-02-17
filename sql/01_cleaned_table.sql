1fc377705df30890a162b26c317b056f8e66d4cb
mxgrtdata <max08.guerout@gmail.com>
2026-02-16 23:09:47 -0500

    Create 01_cleaned_table.sql

---
+++ sql/01_cleaned_table.sql
@@ -0,0 +1,6 @@
+/* Cleaning of the table salesforce_Account */
+
+CREATE OR REPLACE TABLE `data-technical-cases.mguerout.salesforce_account_clean_ex1` AS
+SELECT *
+FROM `data-technical-cases.da_case.salesforce_Account`
+WHERE Paid_Solutions__c IS NOT NULL;
